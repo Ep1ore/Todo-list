4873bdc4e00d9ea786f83e84be31c85b145581ed
Erick <112605408+Ep1ore@users.noreply.github.com>
2024-04-12 20:17:10 -0600

    Move content from index.js to main.js

---
+++ dist/main.js
@@ -0,0 +1,6 @@
+const newBtn = document.getElementById("new-btn");
+newBtn.addEventListener("click", addTodo);
+
+function addTodo() {
+    
+}
--- src/index.js
+++
@@ -1,9 +0,0 @@
-const newBtn = document.getElementById("new-btn");
-newBtn.addEventListener("click");
-
-function addTodo() {
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-
-    
-}
