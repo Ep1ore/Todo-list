9b30f3f709151f5e480ce342f0981c6568dc82e7
Erick <112605408+Ep1ore@users.noreply.github.com>
2024-04-12 20:20:06 -0600

    New Todo button asks user for input

--- dist/main.js
+++ dist/main.js
@@ -2,5 +2,8 @@ const newBtn = document.getElementById("new-btn");
 newBtn.addEventListener("click", addTodo);
 
 function addTodo() {
-    
+    const title = prompt("Title of todo:")
+    const description = prompt("Description:")
+    const dueDate = prompt("Due date of todo (use MM/DD/YYYY) format:")
+    const priority = prompt("Priority of todo (Low, Medium, High):")
 }
