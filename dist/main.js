f0422f8e536745ec1320b2aef7c949538845d610
Erick <112605408+Ep1ore@users.noreply.github.com>
2024-04-15 19:26:44 -0600

    Fixed problems with webpack

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+document.getElementById("new-btn").addEventListener("click",(function(){prompt("Title of todo:"),prompt("Description:"),prompt("Due date of todo (use MM/DD/YYYY) format:"),prompt("Priority of todo (Low, Medium, High):")}));
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const newBtn = document.getElementById("new-btn");
-newBtn.addEventListener("click");
+newBtn.addEventListener("click", addTodo);
 
 function addTodo() {
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-
-    
+    const title = prompt("Title of todo:")
+    const description = prompt("Description:")
+    const dueDate = prompt("Due date of todo (use MM/DD/YYYY) format:")
+    const priority = prompt("Priority of todo (Low, Medium, High):")
 }
